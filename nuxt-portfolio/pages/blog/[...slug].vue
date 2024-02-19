e0b56f1c5adef8b0efc7c2a34bf9e8262ae8fb83
Clifford William <ccliffordwilliam@gmail.com>
2024-02-19 17:45:23 +0800

    update

---
+++ nuxt-portfolio/pages/blog/[...slug].vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>blog detail {{ route.params.slug }}</h1>
+  <article>
+    <ContentDoc />
+  </article>
+</template>
+<script setup>
+const route = useRoute();
+</script>
--- nuxt-portfolio/pages/blog/[id].vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <h1>blog detail {{ route.params.id }}</h1>
-</template>
-<script setup>
-const route = useRoute();
-</script>
